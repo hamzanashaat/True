3629ba1395aef695807a344043256abce2d4733e
DbsaGo <132946411+hamzanashaat@users.noreply.github.com>
2025-07-18 11:53:49 +0300

    Add files via upload

---
+++ yay6.py
@@ -0,0 +1,4 @@
+import numpy as np
+A= np.array([10, 20, 30, 40, 50])
+reshaped_array= A.reshape(5, 1)
+print(reshaped_array)
---
+++ yay7.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+A = np.array([10, 20, 30, 40, 50])
+B = np.array([5, 4, 3, 2, 1])
+
+dot_product = np.dot(A, B)
+
+print("Dot product of A and B is:", dot_product)
