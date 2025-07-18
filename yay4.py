e35221466c993c4007eb9910c1416cbb982e8138
DbsaGo <132946411+hamzanashaat@users.noreply.github.com>
2025-07-18 11:33:25 +0300

    Add files via upload

---
+++ yay4.py
@@ -0,0 +1,4 @@
+import numpy as np
+A= np.array ([10, 20, 30, 40, 50])
+B= np.array ([[5, 4, 3, 2, 1]])
+print(A % B)
---
+++ yay5.py
@@ -0,0 +1,9 @@
+import numpy as np
+A= np.array ([10, 20, 30, 40, 50])
+B= np.array ([[5, 4, 3, 2, 1]])
+print(A.min())
+print(A.max())
+print(A.sum())
+print(B.min())
+print(B.max())
+print(B.sum())
